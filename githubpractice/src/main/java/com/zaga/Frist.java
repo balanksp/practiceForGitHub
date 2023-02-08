e975fdae6ad01c6f0f9a254e9c0eed5b63150827
balanksp <a.bala101@gmail.com>
2023-02-08 12:28:05 +0530

    push the code to repo

---
+++ githubpractice/src/main/java/com/zaga/Frist.java
@@ -0,0 +1,13 @@
+package com.zaga;
+
+public class Frist {
+    /*
+     * this workspace name is new folder for github
+     * java file store in the com.zaga
+     */
+
+     /*
+      * git init cmd is create .git folder in local repo
+      * 
+      */
+}
