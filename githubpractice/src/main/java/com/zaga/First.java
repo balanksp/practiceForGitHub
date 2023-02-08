ac6c29322656dd0eb739c8e67b778468f40e2d33
balanksp <a.bala101@gmail.com>
2023-02-08 14:03:35 +0530

    changed the class name as First

---
+++ githubpractice/src/main/java/com/zaga/First.java
@@ -0,0 +1,21 @@
+package com.zaga;
+
+public class First {
+    /*
+     * this workspace name is new folder for github
+     * java file store in the com.zaga
+     */
+
+     /*
+      * git init cmd is set local repo for store objects and metadata in existing or new dir
+      * 
+      */
+
+      /*
+      * 
+      * 
+      */
+      public static void main(String[] args) {
+        System.out.println("need help");
+      }
+}
