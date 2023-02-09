2da2a9198473d3d702351f58dc12ba9bd06a2430
balanksp <a.bala101@gmail.com>
2023-02-09 10:19:45 +0530

    pull the files from

---
+++ githubpractice/src/main/java/com/zaga/Second.java
@@ -0,0 +1,11 @@
+package com.zaga;
+
+public class Second {
+    public static void main(String[] args) {
+        System.out.println("1. push the statement new_branch01 to new_branch02");
+
+
+    }
+
+    
+}
