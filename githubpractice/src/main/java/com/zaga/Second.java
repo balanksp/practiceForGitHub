ec9f3820248e29dd00187ad355e2ebf09cd3da83
balanksp <a.bala101@gmail.com>
2023-02-09 10:51:28 +0530

    push code from new branch02 to new branch01

--- githubpractice/src/main/java/com/zaga/Second.java
+++ githubpractice/src/main/java/com/zaga/Second.java
@@ -4,7 +4,8 @@ public class Second {
     public static void main(String[] args) {
         System.out.println("1. push the statement new_branch01 to new_branch02");
 
-
+        System.out.println("push the code from new branch02 to new branch01");
+        
     }
 
     
